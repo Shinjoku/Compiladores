9215dcc84ab7ff29c30c677406e2efded86ca622
luizerbetto <felipe_masson123@hotmail.com>
2018-09-05 08:06:41 -0300

    70% das funcoes feitas
    funcao de preenchimento de tabela,
    leitura da mesma e preenchimento das areas de texto.
    falta o break e o rdl

--- GUI/src/gui/GUI.java
+++ GUI/src/gui/GUI.java
@@ -5,6 +5,8 @@
  */
 package gui;
 
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Felipe
@@ -14,8 +16,45 @@ public class GUI {
     /**
      * @param args the command line arguments
      */
+    private static String [] colunsNames = {"ID", "Comando", "Entrada 1", "Entrada 2"};
+    
+    private static Object [][] data = {
+        {1, "ADD", "1", "2"},
+        {2, "SUM", "2", ""},
+        {3, "MOV", "", ""},
+        {4, "MOV2", "", ""},
+        {5, "ADD", "3", "5"},
+        {6, "ADD", "7", "8"}
+    };
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        //new my.mvgui.MVGUI().setVisible(true);
+        
+        GUINovo tela = new GUINovo();
+        System.out.println(tela.getTable().getRowCount());
+        tela.row(data);
+        //tela.entradaPopup();
+        tela.wait();
+        tela.setVisible(true);
+        readTable(tela);
+        //tela.getTable().getModel().setValueAt(data[0][0],0,0);
+    }
+    
+    public static void readTable(GUINovo tela){
+        int cntColum;
+        boolean breakPoint = false;
+        DefaultTableModel model = (DefaultTableModel) tela.tableComandos.getModel();
+        for(cntColum = 0; model.getColumnCount() >= cntColum; cntColum++){
+            breakPoint = (Boolean)model.getValueAt(cntColum, 4);
+            if(breakPoint){
+                System.out.println("Break Point Acionado");
+            }
+            System.out.println("gui.GUI.readTable()"+(String)model.getValueAt(cntColum, 1));
+            /*
+            lê os comandos aqui
+            */            
+        }
     }
     
 }
